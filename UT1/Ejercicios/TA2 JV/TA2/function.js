efb962fef924d2c511340edd61ba21adc4dbc894
Diego Renaldin <diego.renaldin@hotmail.com>
2025-04-02 18:22:09 -0300

    Me habia faltado subir uno

---
+++ UT1/Ejercicios/TA2 JV/TA2/function.js
@@ -0,0 +1,15 @@
+// <!-- TA 2
+// Dado un archivo HTML, crear un archivo .js que implemente una función llamada reverseString(texto: string),
+//  que invierta el párametro texto y lo imprima usando console.log().
+// Pueden usarse diferentes métodos provistos por JavaScript, la idea no es hacer el reverso a mano -->
+
+window.onload = function () {
+    document.body.innerHTML = `
+        <h1>
+            al texto que desee
+        </h1>
+    `;
+};
+
+
+      
